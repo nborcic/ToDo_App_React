e9cdb58d50da165b264056899c30bdc1fa83e599
John Doe <nickborcic69@gmail.com>
2024-05-12 15:41:02 +0200

    just link to GitHub added :*

--- ToDo_App_React/src/components/Footer.jsx
+++ ToDo_App_React/src/components/Footer.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 const Footer = () => {
   return (
     <div className="h-12 w-auto bg-indigo-700 m-0 p-0">
-      <a href="/">
+      <a href="https://github.com/nborcic/ToDo_App_React">
         <button
           className="text-white text-md p-4 justify-center items-center bg-indigo-700 rounded-r-full 
          hover:bg-indigo-600 focus:outline-none focus:ring-2  
