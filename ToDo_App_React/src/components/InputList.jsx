475546006ceed7eae042dbfaf8ae1e28a674027b
John Doe <nickborcic69@gmail.com>
2024-05-24 21:27:15 +0200

    mostly done, optional features can be added

--- ToDo_App_React/src/components/InputList.jsx
+++ ToDo_App_React/src/components/InputList.jsx
@@ -1,19 +1,27 @@
 import React, { useState } from "react";
 
+import { Icon } from "@iconify/react";
+
 function InputList() {
   const [items, setItems] = useState([]);
   const [inputValue, setInputValue] = useState("");
+  const [style, setStyle] = useState({});
 
-  const deleteMe = (index) => {
+  const deleteMe = (id) => {
     const updatedItems = [...items];
-    updatedItems.splice(index, 1);
+    updatedItems.splice(id, 1);
+    setItems(updatedItems);
+  };
+  const lineOverMe = (id) => {
+    const updatedItems = [...items];
+    updatedItems[id].status = "DONE";
     setItems(updatedItems);
   };
   const handleAddItem = (e) => {
     e.preventDefault();
     setItems([
       ...items,
-      { id: items.length, value: inputValue, status: "notDone" },
+      { id: items.index, value: inputValue, status: "not done" },
     ]);
     setInputValue("");
   };
@@ -27,6 +35,7 @@ function InputList() {
         >
           <input
             required
+            maxLength={40}
             type="text"
             placeholder="Add a new item..."
             className="border-2 border-gray-200 focus:outline-none focus:border-indigo-400 w-[50rem] px-4 rounded-lg m-2 p-2 flex justify-between items-center hover:bg-indigo-100 focus:ring-2 focus:ring-indigo-700 focus:ring-opacity-50 transition duration-500 ease-in-out"
@@ -43,21 +52,32 @@ function InputList() {
         </form>
 
         <div className="flex flex-col justify-center items-center m-0 p-0">
-          <div className="flex justify-center items-center w-[40rem] px-4 rounded-lg border-2 border-gray-200 focus:outline-none focus:border-indigo-400">
+          <div className="flex justify-center items-center w-[70rem] px-4 rounded-lg border-2 border-gray-200 focus:outline-none focus:border-indigo-400">
             <ol>
-              {items.map((item, index) => (
+              {items.map((item, id) => (
                 <li
-                  className="bg-white rounded p-2 font-bold flex justify-between items-center hover:bg-indigo-100 focus:outline-none focus:ring-2 focus:ring-indigo-700 focus:ring-opacity-50 w-80 transition duration-500 ease-in-out m-2"
-                  key={item.id + 1}
+                  className="bg-white rounded p-2 font-bold flex justify-between items-center hover:bg-indigo-100 focus:outline-none focus:ring-2 focus:ring-indigo-700 focus:ring-opacity-50 w-100 transition duration-500 ease-in-out m-2 "
+                  key={items.index}
                 >
-                  {item.id} - {item.value} - {item.status}
+                  {id + 1} - {item.value} - {item.status}
+                  <button
+                    type="button"
+                    id="lineOverMe"
+                    className="text-white w-10 p-2 flex bg-indigo-700 rounded-md"
+                    onClick={() => lineOverMe(id)}
+                  >
+                    <Icon icon="mdi:done" className="w-6 h-6 m-0 p-0 " />
+                  </button>
                   <button
                     type="button"
                     id="deleteMe"
-                    className="text-white w-8 p-2 bg-indigo-700 rounded-md"
-                    onClick={() => deleteMe(index)}
+                    className="text-white w-10 p-2 grid float-right bg-indigo-700 rounded-md"
+                    onClick={() => deleteMe(id)}
                   >
-                    X
+                    <Icon
+                      icon="mdi:delete-outline"
+                      className="w-6 h-6 m-0 p-0 "
+                    />
                   </button>
                 </li>
               ))}
